44e28e0370c6990230bd1529362613c565455792
Viktar <viktar.varabei@mail.ru>
2023-07-10 19:25:48 +0300

    ChangeEnvironment

--- src/environments.ts
+++ src/environments.ts
@@ -1,4 +1,4 @@
-const prod = 'http://viktar331-001-site1.btempurl.com';
+const prod = 'https://viktar331-001-site1.btempurl.com';
 const dev = 'https://localhost:7065';
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
