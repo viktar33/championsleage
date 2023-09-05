4041dcd2d1f3a2f25f2c555f95462703ea120350
Viktar <viktar.varabei@mail.ru>
2023-09-05 11:23:58 +0300

    redeploy. change environment

--- src/environments.ts
+++ src/environments.ts
@@ -1,4 +1,4 @@
-const prod = 'https://viktar331-001-site1.btempurl.com';
+const prod = 'https://viktar222-001-site1.dtempurl.com';
 const dev = 'https://localhost:7065';
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
