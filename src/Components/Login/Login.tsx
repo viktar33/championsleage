698a1a074977d3d03b66277265ba9d7c32eeaad2
Viktar <viktar.varabei@mail.ru>
2023-09-05 16:38:31 +0300

    add cors

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -26,7 +26,6 @@ const Login = () => {
         const response = await fetch(
             `${config}/users/token?name=${name}&password=${password}`, {
                 method: 'GET',
-                mode:'no-cors',
                 headers: {
                     'Access-Control-Allow-Origin': '*',
                     'Access-Control-Request-Method': '*'
