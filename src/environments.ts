8408e81e3d39870dd86838093fe254978b991a4d
Viktar <viktar.varabei@mail.ru>
2023-09-19 18:50:07 +0300

    add env

--- src/environments.ts
+++ src/environments.ts
@@ -1,4 +1,4 @@
-const prod = 'https://viktar222-001-site1.dtempurl.com';
+const prod = 'https://testviktor.somee.com/';
 const dev = 'https://localhost:7065';
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
