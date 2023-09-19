d0edf1e9d2757ad9a9ecc54b65f47b5a780b9834
Viktar <viktar.varabei@mail.ru>
2023-09-19 18:55:03 +0300

    add env

--- src/environments.ts
+++ src/environments.ts
@@ -1,4 +1,4 @@
-const prod = 'https://testviktor.somee.com/';
+const prod = 'https://testviktor.somee.com';
 const dev = 'https://localhost:7065';
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
